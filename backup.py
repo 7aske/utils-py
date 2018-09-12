ccbcd7100acad899fa48c7f0c76386974f4ed1e5
Nikola Tasic <ntasic7@gmail.com>
2018-09-12 19:20:07 +0200

    backup

--- backup.py
+++ backup.py
@@ -9,16 +9,10 @@ dstDir = '/media/nikola/External Disk/CODE'
 
 
 def main():
-    username = input('Username:')
-    password = getpass.unix_getpass('Password:')
     if os.path.exists(drive):
         if os.path.exists(dstDir):
             shutil.rmtree(dstDir)
         shutil.copytree(srcDir, dstDir, ignore=ignore_list)
-
-    if len(password) > 0 and len(username) > 0:
-        os.system(
-            f'smbclient //192.168.1.12/home -W WORKGROUP -U {username} --pass {password} -c \'prompt OFF; recurse ON; cd /home/pi/Documents/CODE; lcd /media/nikola/External" "Disk/CODE; mput *\'')
 
 
 def ignore_patterns(name, path):
