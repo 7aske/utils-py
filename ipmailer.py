6d0515a43080e29f3d893752153af7e29983171e
Nikola Tasic <ntasic7@gmail.com>
2018-11-06 10:56:18 +0100

    ipmailer

--- ipmailer.py
+++ ipmailer.py
@@ -1,9 +1,10 @@
 import smtplib
-import ipaddress
 from requests import get
 from requests.exceptions import ConnectionError
 from threading import Timer
-from sys import argv
+from sys import argv, platform
+from time import strftime, gmtime
+from getpass import unix_getpass, win_getpass
 
 
 class Mailer():
@@ -14,14 +15,10 @@ class Mailer():
     delay = 60
 
     def __init__(self):
+        if len(argv) == 2:
+            self.delay = int(argv[1])
 
-        if len(argv) == 1:
-            raise SystemExit('Password missing.')
-
-        if len(argv) == 3:
-            self.delay = int(argv[2])
-
-        self.password = argv[1]
+        self.password = unix_getpass("Enter password: ") if platform == "linux" else win_getpass("Enter password: ")
         self.ip = self.get_ip()
         self.check_ip_change()
 
@@ -33,20 +30,24 @@ class Mailer():
         return ip
 
     def check_ip_change(self):
-        Timer(self.delay, self.check_ip_change).start()
+        timer = Timer(self.delay, self.check_ip_change)
+        timer.start()
         new_ip = self.get_ip()
-        print(new_ip)
+        time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        print("IP:%s\nTime:%s" % (new_ip, time))
         if new_ip != self.ip:
             self.ip = new_ip
-            self.send_email(self.ip)
+            self.send_email(self.ip, time)
 
-    def send_email(self, ip):
+    def send_email(self, ip, time):
+        text = "New IP address is: %s\nTime of change: %s" % (ip, time)
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(self.username, self.password)
-        server.sendmail(self.username, self.to, ip)
+        server.sendmail(self.username, self.to, text)
         server.quit()
+        print("Mail sent")
 
 
 if __name__ == "__main__":
-    mailer = Mailer()
+    Mailer()
