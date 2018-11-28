7818eeab79dba5d6127d6e513f162a35dd34b551
Nikola Tasic <ntasic7@gmail.com>
2018-11-28 10:29:05 +0100

    autotank update

--- autotank.py
+++ autotank.py
@@ -1,40 +1,49 @@
 from pynput.keyboard import Key, Controller as KeyboardController
 from pynput.mouse import Button, Controller as MouseController
 from pynput.keyboard import Key, Listener as KeyboardListener
+from pynput.mouse import Button, Listener as MouseListener
 from time import sleep
 from threading import Thread
-from win32gui import GetWindowText, GetForegroundWindow
-
-ls = KeyboardListener()
-kb = KeyboardController()
-ms = MouseController()
+from win32gui import GetWindowText, GetForegroundWindow, SetPixel, GetDC
+from win32api import RGB, GetSystemMetrics
 
 run = False
 pause = False
 breaks = [Key.ctrl_l, Key.alt_l, Key.shift_l]
+macros = ['q', 'e', (0, 1), (0, -1), Key.tab]
 
 
 class ScriptController(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
+        kl = KeyboardListener()
+        ml = MouseListener()
         self.start()
+
         with KeyboardListener(on_press=self.on_press, on_release=self.on_release) as listener:
             listener.join()
 
     def on_press(self, key):
         global breaks, pause
+
         try:
             if key in breaks:
                 pause = True
+
             if key.char == "t":
                 global run
                 run = not run
+
                 if run:
-                    print("Staring\n")
+                    print("\nRun")
+
                 else:
-                    print("Stoping\n")
+                    print("\nPause")
+
+
             elif key.char == "\\":
+                print("Exiting")
                 raise SystemExit("Exiting")
 
         except Exception as e:
@@ -43,7 +52,7 @@ class ScriptController(Thread):
     def on_release(self, key):
         global breaks, pause
         try:
-            if key in breaks and not self.test_window():
+            if key in breaks:
                 pause = False
 
         except Exception as e:
@@ -67,28 +76,43 @@ class ScriptController(Thread):
 
 class Keypresser(Thread):
     gcd = 1
+    kc = KeyboardController()
+    mc = MouseController()
+    c = RGB(255, 0, 0)
+    dc = GetDC(0)
 
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
         self.start()
 
+    def indicator(self):
+        width = int(GetSystemMetrics(0)/2)
+        for i in range(width - 12, width + 12):
+            for j in range(24):
+                SetPixel(self.dc, i, j, self.c)
+
     def run(self):
         while True:
-
-            sleep(0.1)
             global run, pause
-            if run and not pause:
-                kb.press('q')
-                kb.release('q')
-                kb.press('e')
-                kb.release('e')
-                ms.scroll(0, 1)
-                ms.scroll(0, -1)
-                kb.press(Key.tab)
-                kb.release(Key.tab)
+
+            if run:
+                self.indicator()
+
+                if not pause:
+                    for macro in macros:
+                        if isinstance(macro, tuple):
+                            print(macro[0], macro[1])
+                            self.mc.scroll(macro[0], macro[1])
+
+                        else:
+                            self.kc.press(macro)
+                            self.kc.release(macro)
+
+                        sleep(0.1)
                 sleep(self.gcd)
 
 
-Keypresser()
-ScriptController()
+if __name__ == "__main__":
+    Keypresser()
+    ScriptController()
