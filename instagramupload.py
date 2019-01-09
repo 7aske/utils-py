d70200602e8c09f642aea6b0cc4b9be1e68ed410
Nikola Tasic <ntasic7@gmail.com>
2019-01-09 04:39:18 +0100

    argv formatting and ini file implementation

--- instagramupload.py
+++ instagramupload.py
@@ -1,11 +1,12 @@
 from os import listdir, getcwd, remove
-from os.path import join, splitext
+from os.path import join, splitext, exists
 from PIL import Image
 from sys import argv
 from instapy_cli import client
 from time import sleep
 from random import randrange, choice
 import getpass
+import configparser
 
 
 class Stack:
@@ -29,94 +30,135 @@ class Stack:
 
 
 class Main:
-    username = ""
-    password = ""
-    bnw_caption = "#blackandwhitephotography #blackandwhite #streetphotography_bw #bw #bnw #bnwmood #bnw_captures #bnwphotography"
-    regular_caption = "#vscofilm #vscodaily #vscocam #vsco #vscogood #vscoph #vsco_rs  #vscogrid #vscomasters #vscobalkan #photography #fuji #explore #street #streetphotography #urban #urbanexploring #people #photojournalism"
-    photos = Stack()
-    photos_dir = ""
-    timeout = 0
-    watch = False
+    __username = ""
+    __password = ""
+    __watch = False
+    __photos = Stack()
+    __photos_dir = ""
+    __config = configparser.ConfigParser()
+    __config_path = join(getcwd(), "config.ini")
+    _bnw_caption = "#blackandwhitephotography #blackandwhite #streetphotography_bw #bw #bnw #bnwmood #bnw_captures #bnwphotography"
+    _regular_caption = "#vscofilm #vscodaily #vscocam #vsco #vscogood #vscoph #vsco_rs #vscogrid #vscomasters #vscobalkan #photography #fuji #explore #street #streetphotography #urban #urbanexploring #people #photojournalism"
+    _timeout = 0
+
     def __init__(self):
+
         if "--watch" in argv:
-            self.watch = True
+            self.__watch = True
             print("Starting in watch mode.")
+            argv.remove("--watch")
         possible_answers = ["Y", "N"]
         answer = ""
-        self.username = input("Username: ")
-        self.password = getpass.unix_getpass("Password: ")
+        if exists(self.__config_path):
+            self.__config.read(self.__config_path)
+            if "credentials" not in self.__config:
+                raise SystemExit("Bad config file.")
+            else:
+                if "username" not in self.__config["credentials"] or "password" not in self.__config["credentials"]:
+                    raise SystemExit("Bad config file.")
 
-        if len(argv) in [3, 4]:
-            self.timeout = int(argv[2])
-            self.photos_dir = join(getcwd(), argv[1])
-        elif len(argv) == 2:
-            self.timeout = 43200
-            self.photos_dir = join(getcwd(), argv[1])
+            self.__config.read(self.__config_path)
+            self.__username = self.__config["credentials"]["username"]
+            self.__password = self.__config["credentials"]["password"]
+            print("Account: %s" % self.__username)
+            print("Password: %s" % "".join(["*" for _ in self.__password]))
         else:
-            self.timeout = 43200
-            self.photos_dir = getcwd()
+            self.__username = input("Username: ")
+            self.__password = getpass.unix_getpass("Password: ")
+            self.__config["credentials"] = {
+                "username": self.__username,
+                "password": self.__password
+            }
+            with open(self.__config_path, "w") as configfile:
+                self.__config.write(configfile)
+                configfile.close()
+
+        try:
+
+            if "-f" in argv:
+                self.__photos_dir = join(getcwd(), argv[argv.index("-f") + 1])
+            else:
+                self.__photos_dir = getcwd()
+            if "-t" in argv:
+                try:
+                    self._timeout = int(argv[argv.index("-t") + 1])
+                except ValueError:
+                    self._timeout = 43200
+            else:
+                self._timeout = 43200
+        except Exception:
+            raise SystemExit("Usage: -f <folder> -t <timeout> [--watch]")
+
+        if not exists(self.__photos_dir):
+            raise SystemExit("Photos directory doesn't exist.\n%s" % self.__photos_dir)
+
         while answer.upper() not in possible_answers:
-            print("Start uploading from:\n%s" % self.photos_dir)
-            answer = input("Are you sure (Y/N)?")
+            print("Start uploading from: '%s'" % self.__photos_dir, end="")
+            print(" with timeout of '%d'" % self._timeout)
+            answer = input("Are you sure? (Y/N) ")
         if answer.upper() == "Y":
             self.update_photos()
             while True:
-                print(len(self.photos))
-                if len(self.photos) == 0:
+                print("Photos - %d" % len(self.__photos))
+                if len(self.__photos) == 0:
                     self.update_photos()
-                    print("Timeout: " + str(min(3600,self.timeout)))
-                    sleep(min(3600,self.timeout))
+                    print("Timeout - %d" % min(3600, self._timeout))
+                    sleep(min(3600, self._timeout))
                 else:
+                    self.update_tags()
                     self.upload_photo()
                     s = self.get_timeout()
-                    print("Timeout: " + str(s))
+                    print("Timeout - %d" % s)
                     sleep(s)
         else:
-            SystemExit("Bye")
+            raise SystemExit("Bye")
 
     def update_photos(self):
-        for photo in listdir(self.photos_dir):
+        for photo in listdir(self.__photos_dir):
             _, ext = splitext(photo)
             if ext.upper() == ".JPG":
-                self.photos.push(join(self.photos_dir, photo))
+                self.__photos.push(join(self.__photos_dir, photo))
 
-        if len(self.photos) == 0:
-            pass
+        if len(self.__photos) == 0:
+            if not self.__watch:
+                raise SystemExit("Folder Empty")
+
+    def update_tags(self):
+        bnw_path = join(getcwd(), "bnw_tags.txt")
+        regular_path = join(getcwd(), "regular_tags.txt")
+        if exists(bnw_path):
+            with open(bnw_path, "r") as bnw:
+                self._bnw_caption = ""
+                for line in bnw.readlines():
+                    self._bnw_caption += line
+        if exists(regular_path):
+            with open(bnw_path, "r") as regular_path:
+                self._regular_caption = ""
+                for line in regular_path.readlines():
+                    self._regular_caption += line
 
     def upload_photo(self):
-        photo = self.photos.pop()
-        caption = self.regular_caption
+        photo = self.__photos.pop()
+        caption = self._regular_caption
         if self.is_bnw(photo):
-            caption += "\n\n" + self.bnw_caption
-        # TODO: FIX DIS
-        with client(self.username, self.password) as cli:
+            caption += "\n\n" + self._bnw_caption
+        with client(self.__username, self.__password) as cli:
             try:
+                print(caption)
                 cli.upload(photo, caption)
                 remove(photo)
             except Exception as e:
-                if self.watch:
-                    pass
-                else:
-                    raise SystemExit("Folder Empty")
+                pass
 
     def get_timeout(self):
-        offset = choice([-1, 1]) * randrange(int(self.timeout / 20), int(self.timeout / 10) + 1)
-        print(offset)
-        return self.timeout + offset
+        offset = choice([-1, 1]) * randrange(int(self._timeout / 20), int(self._timeout / 10) + 1)
+        return self._timeout + offset
 
-    def is_bnw(self, path):
+    @staticmethod
+    def is_bnw(path):
         img = Image.open(path)
-        pix = {"B": 0, "C": 0, "T": 0}
         w, h = img.size
-        if w < h:
-            if w / h < 0.8:
-                blank = Image.new("RGB", (h, h), color=(255, 255, 255))
-                new_img = blank.copy()
-                offset = int((h - w) / 2)
-                pos = (offset, 0)
-                new_img.paste(img, pos)
-                new_img.save(path)
-
+        pix = {"B": 0, "C": 0, "T": 0}
         for i in range(int(w / 4)):
             for j in range(int(h / 4)):
                 r, g, b = img.getpixel((i * 4, j * 4))
@@ -125,12 +167,26 @@ class Main:
                 else:
                     pix["B"] += 1
                 pix["T"] += 1
+
+        if w < h:
+            if w / h < 0.8:
+                blank = Image.new("RGB", (h, h), color=(255, 255, 255))
+                new_img = blank.copy()
+                offset = int((h - w) / 2)
+                pos = (offset, 0)
+                new_img.paste(img, pos)
+                new_img.save(path)
+                new_img.close()
+                blank.close()
+
         try:
             out = pix["C"] / pix["T"]
+            return out < 0.2
         except ZeroDivisionError:
             out = 0
-        print(out)
-        return out < 0.2
+            return out < 0.2
+        finally:
+            img.close()
 
 
 if __name__ == '__main__':
