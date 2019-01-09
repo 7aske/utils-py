6629e05808d529766bf291c062c54a59fafdc177
Nikola Tasic <ntasic7@gmail.com>
2019-01-09 02:52:56 +0100

    sleepfix

--- instagramupload.py
+++ instagramupload.py
@@ -64,6 +64,8 @@ class Main:
                 print(len(self.photos))
                 if len(self.photos) == 0:
                     self.update_photos()
+                    print("Timeout: " + min(3600,self.timeout))
+                    sleep(min(3600,self.timeout))
                 else:
                     self.upload_photo()
                     s = self.get_timeout()
